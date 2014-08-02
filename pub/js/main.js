10275ae621c89ef6e165b265f9dab801cfb3ce5a
Matt Webb <matt.webb@sympoz.com>
2014-08-02 16:22:18 -0600

    adding js

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,3 +1,11 @@
-(function($, window, document, undefined) {
+  function githubUser() {
+    var user = $('#userName').val();
+    $.post('http://localhost:8080/api/', {userName: user}, function (data) {
+      console.log(data);
 
-})(jQuery, this, this.document);
+      $('#github-image').attr('src', data.avatar_url);
+      $('#github-name').text(data.login);
+      $('#github-description').text(data.location);
+      $('#github-link').attr('href', data.html_url);
+    });
+}
