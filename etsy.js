c282b98c30210c916ea51862f823d0ab80b8ac37
Thabo <tha.bo.fletcher@gmail.com>
2014-08-02 15:21:56 -0700

    shakjsdfha

--- etsy.js
+++ etsy.js
@@ -7,7 +7,15 @@ exports.getMatch = function(ghName, cb) {
 
 	req.get('https://openapi.etsy.com/v2/users?keywords=' + ghName + '&api_key=20dq535j4h6pc5qdbpqnqed4',function (error, response) {
 	    if (!error && response.statusCode == 200) {
-	    	cb(response.body)
+
+	    	var users = JSON.parse(response.body).results
+
+	    	console.log(users)
+
+	    	req.get('https://openapi.etsy.com/v2/users/' + users[0].user_id + '/profile?api_key=20dq535j4h6pc5qdbpqnqed4',function (error, response) {
+	    		cb(JSON.parse(response.body))
+	    	})
+	    	
 	    }
 	})
 	// req.get('https://openapi.etsy.com/v2/users/etsystore?api_key=your_api_key')
