e7250ab62cdc175482755fd4846aa562f6081ca2
Thabo <tha.bo.fletcher@gmail.com>
2014-08-02 15:14:46 -0700

    etsy data

--- etsy.js
+++ etsy.js
@@ -3,11 +3,11 @@ var req = require('request')
 // var Etsy = require('etsy').Etsy;
 // var api = new Etsy('20dq535j4h6pc5qdbpqnqed4', 'fahr27dspe'); 
 
-exports.getMatch = function(ghName) {
+exports.getMatch = function(ghName, cb) {
 
-	req.get('https://openapi.etsy.com/v2/users?keywords=' + 'thabo' + '&api_key=20dq535j4h6pc5qdbpqnqed4',function (error, response, body) {
+	req.get('https://openapi.etsy.com/v2/users?keywords=' + 'thabo' + '&api_key=20dq535j4h6pc5qdbpqnqed4',function (error, response) {
 	    if (!error && response.statusCode == 200) {
-	      //console.log(users.results[0]);
+	    	cb(response.body)
 	    }
 	})
 	// req.get('https://openapi.etsy.com/v2/users/etsystore?api_key=your_api_key')
