e0dc62323282b222676fcefee7ba7c400ba39665
Thabo <tha.bo.fletcher@gmail.com>
2014-08-02 14:13:11 -0700

    basic server

---
+++ api.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var https = require('https');
+
+var fb = require('firebase');
+var cojack = new fb('https://cojack.firebaseio.com/');
+//chats.auth('5V0BdAPl4cUicwHGCTy45O4gNBPjWkZ8bp3FmgyV');
+
+
+exports.post = function(req, res) {
+}
