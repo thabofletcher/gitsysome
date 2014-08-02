25694177c3b3ed61e2552c6a29c83745fe0e4c8f
Thabo <tha.bo.fletcher@gmail.com>
2014-08-02 14:57:16 -0700

    get rid of etsy

--- etsy.js
+++ etsy.js
@@ -1,12 +1,16 @@
+var req = require('request')
 
-var Etsy = require('etsy').Etsy;
-var api = new Etsy('20dq535j4h6pc5qdbpqnqed4', 'fahr27dspe'); 
-
+// var Etsy = require('etsy').Etsy;
+// var api = new Etsy('20dq535j4h6pc5qdbpqnqed4', 'fahr27dspe'); 
 
 exports.getMatch = function(ghName) {
-	api.findAllUsers({
-	    keywords: 'a',
-	}, function(err, users) {
-	    console.log(users.results[0]);
-	});	
+
+	// req.get('https://openapi.etsy.com/v2/users/etsystore?api_key=your_api_key')
+
+
+	// api.findAllUsers({
+	//     keywords: 'a',
+	// }, function(err, users) {
+	//     console.log(users.results[0]);
+	// });	
 }
