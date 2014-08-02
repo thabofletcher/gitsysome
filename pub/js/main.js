a213bc03b22053c80b1691e39dc06a32d2821f88
Matt Webb <matt.webb@sympoz.com>
2014-08-02 16:30:55 -0600

    shit

--- pub/js/main.js
+++ pub/js/main.js
@@ -3,9 +3,15 @@
     $.post('http://localhost:8080/api/', {userName: user}, function (data) {
       console.log(data);
 
-      $('#github-image').attr('src', data.avatar_url);
-      $('#github-name').text(data.login);
-      $('#github-description').text(data.location);
-      $('#github-link').attr('href', data.html_url);
+      $('#github-image').attr('src', data.gh.avatar_url);
+      $('#github-name').text(data.gh.login);
+      $('#github-description').text(data.gh.location);
+      $('#github-link').attr('href', data.gh.html_url);
+
+
+      $('#etsy-image').attr('src', data.etsy.results[0].avatar_url);
+      $('#etsy-name').text(data.etsy.results[0].login);
+      $('#etsy-description').text(data.etsy.results[0].location);
+      $('#etsy-link').attr('href', data.etsy.results[0].html_url);
     });
 }
